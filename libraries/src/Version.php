56510413c1d9b48e6d72772f3bfa207f2b9e3359
Stefan Wendhausen <stefan.wendhausen@tec-promotion.de>
2026-06-27 20:51:28 +0200

    Prepare Joomla! 6.1.2

--- libraries/src/Version.php
+++ libraries/src/Version.php
@@ -66,7 +66,7 @@ final class Version
      * @var    string
      * @since  3.8.0
      */
-    public const EXTRA_VERSION = 'rc3-dev';
+    public const EXTRA_VERSION = 'rc2+security';
 
     /**
      * Development status.
@@ -74,7 +74,7 @@ final class Version
      * @var    string
      * @since  3.5
      */
-    public const DEV_STATUS = 'Development';
+    public const DEV_STATUS = 'Release Candidate';
 
     /**
      * Code name.
@@ -98,7 +98,7 @@ final class Version
      * @var    string
      * @since  3.5
      */
-    public const RELTIME = '18:31';
+    public const RELTIME = '18:30';
 
     /**
      * Release timezone.
